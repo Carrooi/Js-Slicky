8cbe48b83c45822cdaec3567aed77bf26dc0d638
David Kudera <kudera.d@gmail.com>
2016-06-05 22:08:55 +0200

    Compiler: optimize directives parsing

--- src/Compiler.ts
+++ src/Compiler.ts
@@ -101,56 +101,10 @@ export class Compiler
 
 	public compileElement(parentView: View, el: HTMLElement): void
 	{
-		let parseInner = true;
-
-		if (Compiler.IGNORED_ELEMENTS.indexOf(el.nodeName.toUpperCase()) > -1) {
-			parseInner = false;
-		}
-
-		let directives: Array<{directive: any, metadata: DirectiveMetadataDefinition, definition: DirectiveDefinition, instance: any}> = [];
-		let components: Array<string> = [];
-
 		let elementRef = ElementRef.getByNode(el);
 		let view = View.getByElement(elementRef, parentView);
 
 		let attributes = elementRef.getAttributes();
-
-		for (let i = 0; i < view.directives.length; i++) {
-			let directive = view.directives[i];
-			let isComponent = false;
-			let metadata: DirectiveMetadataDefinition;
-			let definition: DirectiveDefinition;
-
-			if (Annotations.hasAnnotation(directive, ComponentMetadataDefinition)) {
-				isComponent = true;
-				metadata = ControllerParser.getControllerMetadata(directive);
-				definition = ControllerParser.parse(directive, <ComponentMetadataDefinition>metadata);
-			} else {
-				metadata = DirectiveParser.getDirectiveMetadata(directive);
-				definition = DirectiveParser.parse(directive, metadata);
-			}
-
-			if (Dom.matches(el, metadata.selector)) {
-				if (isComponent) {
-					components.push(Functions.getName(directive));
-				}
-
-				let instance = this.createDirective(view, definition, el);
-
-				view.attachDirective(definition, instance);
-
-				directives.push({
-					directive: directive,
-					metadata: metadata,
-					definition: definition,
-					instance: instance,
-				});
-			}
-		}
-
-		if (components.length > 1) {
-			throw new Error('Can not attach more than 1 components (' + components.join(', ') + ') to ' + Dom.getReadableName(el) + ' element.');
-		}
 
 		for (let i = 0; i < attributes.length; i++) {
 			let attr = attributes[i];
@@ -182,152 +136,55 @@ export class Compiler
 			}
 		}
 
-		let innerCompilationNeeded = true;
-		for (let i = 0; i < directives.length; i++) {
-			let definition = directives[i].definition;
-			let instance = directives[i].instance;
+		let components: Array<string> = [];
+		let innerCompilationNeeded = false;
+		let directiveExists = false;
 
-			let hasOnChange = typeof instance['onChange'] === 'function';
-			let hasOnUpdate = typeof instance['onUpdate'] === 'function';
+		for (let i = 0; i < view.directives.length; i++) {
+			let directive = view.directives[i];
+			let isComponent = false;
+			let metadata: DirectiveMetadataDefinition;
+			let definition: DirectiveDefinition;
 
-			((instance, definition, hasOnChange, hasOnUpdate) => {
-				let processInput = (inputName: string, required: boolean, expr: Expression, changed: Array<ChangedObject> = null) => {
-					let stop = false;
+			if (Annotations.hasAnnotation(directive, ComponentMetadataDefinition)) {
+				isComponent = true;
+				metadata = ControllerParser.getControllerMetadata(directive);
+				definition = ControllerParser.parse(directive, <ComponentMetadataDefinition>metadata);
+			} else {
+				metadata = DirectiveParser.getDirectiveMetadata(directive);
+				definition = DirectiveParser.parse(directive, metadata);
+			}
 
-					if (hasOnChange) {
-						stop = (<OnChange>instance).onChange(inputName, changed) === false;
-					}
+			if (Dom.matches(el, metadata.selector)) {
+				directiveExists = true;
 
-					if (!stop) {
-						let value = ExpressionParser.parse(expr, view.parameters);
-
-						instance[inputName] = value;
-
-						if (hasOnUpdate) {
-							(<OnUpdate>instance).onUpdate(inputName, value);
-						}
-					}
-				};
-
-				for (let inputName in definition.inputs) {
-					if (definition.inputs.hasOwnProperty(inputName)) {
-						let input = definition.inputs[inputName];
-						let realInputName = (input.name ? input.name : inputName).toLowerCase();
-
-						let attr = elementRef.getAttribute(realInputName);
-
-						if (!attr) {
-							if (typeof el[realInputName] === 'undefined') {
-								if (input.required) {
-									throw new Error('Component\'s input ' + Functions.getName(definition.directive) + '::' + inputName + ' was not found in ' + Dom.getReadableName(<Element>el) + ' element.');
-								} else if (typeof instance[inputName] !== 'undefined') {
-									continue;
-								}
-							}
-
-							instance[inputName] = el[realInputName];
-
-						} else {
-							if (attr.property) {
-								let expr = ExpressionParser.precompile(attr.expression);
-
-								processInput(inputName, input.required, expr);
-
-								((inputName, required, expr) => {
-									view.watch(expr, (changed) => {
-										processInput(inputName, required, expr, changed);
-									});
-								})(inputName, input.required, expr);
-							} else {
-								instance[inputName] = attr.expression;
-							}
-
-							attr.bound = true;
-						}
-					}
+				if (isComponent) {
+					components.push(Functions.getName(directive));
 				}
-			})(instance, definition, hasOnChange, hasOnUpdate);
 
-			for (let i = 0; i < attributes.length; i++) {
-				if (attributes[i].property && !attributes[i].bound) {
-					throw new Error('Could not bind property ' + attributes[i].name + ' to element ' + Dom.getReadableName(el) + ' or to any of its directives.');
+				let instance = this.createDirective(view, definition, el);
+
+				view.attachDirective(definition, instance);
+
+				let innerCompiled = this.processDirective(el, view, elementRef, definition, instance);
+
+				if (!innerCompiled && definition.metadata.compileInner) {
+					innerCompilationNeeded = true;
 				}
 			}
+		}
 
-			if (definition.metadata instanceof ComponentMetadataDefinition && (<ComponentMetadataDefinition>definition.metadata).template) {
-				el.innerHTML = (<ComponentMetadataDefinition>definition.metadata).template;
-				innerCompilationNeeded = true;
-			}
+		if (components.length > 1) {
+			throw new Error('Can not attach more than 1 components (' + components.join(', ') + ') to ' + Dom.getReadableName(el) + ' element.');
+		}
 
-			if (el.innerHTML !== '' && definition.metadata.compileInner && innerCompilationNeeded) {
-				this.compileNodes(view, el.childNodes);
-			}
-
-			innerCompilationNeeded = false;
-
-			if (definition.metadata instanceof ComponentMetadataDefinition) {
-				let elements = (<ControllerDefinition>definition).elements;
-				for (let elementName in elements) {
-					if (elements.hasOwnProperty(elementName)) {
-						let element = elements[elementName];
-
-						if (element.selector) {
-							let subElements = Dom.querySelectorAll(element.selector, el);
-
-							if (subElements.length === 0) {
-								instance[elementName] = null;
-
-							} else if (subElements.length === 1) {
-								instance[elementName] = subElements[0];
-
-							} else {
-								instance[elementName] = subElements;
-							}
-						} else {
-							instance[elementName] = el;
-						}
-					}
-				}
-			}
-
-			for (let eventName in definition.events) {
-				if (definition.events.hasOwnProperty(eventName)) {
-					let event = definition.events[eventName];
-
-					if (event.el === '@') {
-						Dom.addEventListener(el, event.name, instance, instance[eventName]);
-
-					} else {
-						if (typeof event.el === 'string' && (<string>event.el).substr(0, 1) === '@') {
-							let childName = (<string>event.el).substr(1);
-							if (typeof instance[childName] === 'undefined') {
-								throw new Error('Can not add event listener for @' + childName + ' at ' + Functions.getName(instance));
-							}
-
-							Dom.addEventListener(instance[childName], event.name, instance, instance[eventName]);
-
-						} else if (typeof event.el === 'string') {
-							let eventEls = Dom.querySelectorAll(<string>event.el, el);
-							for (let j = 0; j < eventEls.length; j++) {
-								Dom.addEventListener(eventEls[j], event.name, instance, instance[eventName]);
-							}
-
-						} else if (event.el instanceof Window || event.el instanceof Node) {
-							Dom.addEventListener(<Node>event.el, event.name, instance, instance[eventName]);
-
-						}
-					}
-				}
-			}
-
-			if (typeof instance['onInit'] === 'function') {
-				(<OnInit>instance).onInit();
-			}
+		if (!directiveExists) {
+			innerCompilationNeeded = true;
 		}
 
 		Dom.removeCssClass(el, Compiler.CLOAK_CSS_CLASS);
 
-		if (parseInner && innerCompilationNeeded) {
+		if (Compiler.IGNORED_ELEMENTS.indexOf(el.nodeName.toUpperCase()) === -1 && innerCompilationNeeded) {
 			this.compileNodes(view, el.childNodes);
 		}
 	}
@@ -430,6 +287,151 @@ export class Compiler
 		}
 
 		return parent
+	}
+
+
+	private processDirective(el: HTMLElement, view: View, elementRef: ElementRef, definition: DirectiveDefinition, instance: any): boolean
+	{
+		let attributes = elementRef.getAttributes();
+
+		let innerCompiled = false;
+
+		let hasOnChange = typeof instance['onChange'] === 'function';
+		let hasOnUpdate = typeof instance['onUpdate'] === 'function';
+
+		((instance, definition, hasOnChange, hasOnUpdate) => {
+			let processInput = (inputName: string, required: boolean, expr: Expression, changed: Array<ChangedObject> = null) => {
+				let stop = false;
+
+				if (hasOnChange) {
+					stop = (<OnChange>instance).onChange(inputName, changed) === false;
+				}
+
+				if (!stop) {
+					let value = ExpressionParser.parse(expr, view.parameters);
+
+					instance[inputName] = value;
+
+					if (hasOnUpdate) {
+						(<OnUpdate>instance).onUpdate(inputName, value);
+					}
+				}
+			};
+
+			for (let inputName in definition.inputs) {
+				if (definition.inputs.hasOwnProperty(inputName)) {
+					let input = definition.inputs[inputName];
+					let realInputName = (input.name ? input.name : inputName).toLowerCase();
+
+					let attr = elementRef.getAttribute(realInputName);
+
+					if (!attr) {
+						if (typeof el[realInputName] === 'undefined') {
+							if (input.required) {
+								throw new Error('Component\'s input ' + Functions.getName(definition.directive) + '::' + inputName + ' was not found in ' + Dom.getReadableName(<Element>el) + ' element.');
+							} else if (typeof instance[inputName] !== 'undefined') {
+								continue;
+							}
+						}
+
+						instance[inputName] = el[realInputName];
+
+					} else {
+						if (attr.property) {
+							let expr = ExpressionParser.precompile(attr.expression);
+
+							processInput(inputName, input.required, expr);
+
+							((inputName, required, expr) => {
+								view.watch(expr, (changed) => {
+									processInput(inputName, required, expr, changed);
+								});
+							})(inputName, input.required, expr);
+						} else {
+							instance[inputName] = attr.expression;
+						}
+
+						attr.bound = true;
+					}
+				}
+			}
+		})(instance, definition, hasOnChange, hasOnUpdate);
+
+		for (let i = 0; i < attributes.length; i++) {
+			if (attributes[i].property && !attributes[i].bound) {
+				throw new Error('Could not bind property ' + attributes[i].name + ' to element ' + Dom.getReadableName(el) + ' or to any of its directives.');
+			}
+		}
+
+		if (definition.metadata instanceof ComponentMetadataDefinition && (<ComponentMetadataDefinition>definition.metadata).template) {
+			el.innerHTML = (<ComponentMetadataDefinition>definition.metadata).template;
+		}
+
+		if (el.innerHTML !== '' && definition.metadata.compileInner) {
+			this.compileNodes(view, el.childNodes);
+			innerCompiled = true;
+		}
+
+		if (definition.metadata instanceof ComponentMetadataDefinition) {
+			let elements = (<ControllerDefinition>definition).elements;
+			for (let elementName in elements) {
+				if (elements.hasOwnProperty(elementName)) {
+					let element = elements[elementName];
+
+					if (element.selector) {
+						let subElements = Dom.querySelectorAll(element.selector, el);
+
+						if (subElements.length === 0) {
+							instance[elementName] = null;
+
+						} else if (subElements.length === 1) {
+							instance[elementName] = subElements[0];
+
+						} else {
+							instance[elementName] = subElements;
+						}
+					} else {
+						instance[elementName] = el;
+					}
+				}
+			}
+		}
+
+		for (let eventName in definition.events) {
+			if (definition.events.hasOwnProperty(eventName)) {
+				let event = definition.events[eventName];
+
+				if (event.el === '@') {
+					Dom.addEventListener(el, event.name, instance, instance[eventName]);
+
+				} else {
+					if (typeof event.el === 'string' && (<string>event.el).substr(0, 1) === '@') {
+						let childName = (<string>event.el).substr(1);
+						if (typeof instance[childName] === 'undefined') {
+							throw new Error('Can not add event listener for @' + childName + ' at ' + Functions.getName(instance));
+						}
+
+						Dom.addEventListener(instance[childName], event.name, instance, instance[eventName]);
+
+					} else if (typeof event.el === 'string') {
+						let eventEls = Dom.querySelectorAll(<string>event.el, el);
+						for (let j = 0; j < eventEls.length; j++) {
+							Dom.addEventListener(eventEls[j], event.name, instance, instance[eventName]);
+						}
+
+					} else if (event.el instanceof Window || event.el instanceof Node) {
+						Dom.addEventListener(<Node>event.el, event.name, instance, instance[eventName]);
+
+					}
+				}
+			}
+		}
+
+		if (typeof instance['onInit'] === 'function') {
+			(<OnInit>instance).onInit();
+		}
+
+		return innerCompiled;
 	}
 
 }
