74f5f5237f3d054fd8d30565bdcd569fdf5dc9a4
David Kudera <kudera.d@gmail.com>
2016-11-27 20:25:13 +0100

    Refactoring: uncomment ClassGenerator tests

--- tests/tests/Util/CodeGenerator/ClassGenerator.ts
+++ tests/tests/Util/CodeGenerator/ClassGenerator.ts
@@ -8,20 +8,28 @@ let expect = chai.expect;
 
 describe('#Util/CodeGenerator/ClassGenerator', () => {
 
-	describe.skip('toString()', () => {
+	describe('toString()', () => {
 
 		it('should generate simple class', () => {
 			let generator = new ClassGenerator('Test');
-			expect(generator.toString()).to.be.equal('function Test() {}');
+			expect(generator.toString()).to.be.equal(
+				'return (function() {\n' +
+					'\tfunction Test() {};\n' +
+					'\treturn Test;\n' +
+				'})();'
+			);
 		});
 
 		it('should generate class with constructor', () => {
 			let generator = new ClassGenerator('Test');
 			generator.setConstructor(['arg1', 'arg2', 'arg3'], 'this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3;');
 			expect(generator.toString()).to.be.equal(
-				'function Test(arg1, arg2, arg3) {' +
-					'this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3;' +
-				'}'
+				'return (function() {\n' +
+					'\tfunction Test(arg1, arg2, arg3) {\n' +
+						'\t\tthis.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3;\n' +
+					'\t};\n' +
+					'\treturn Test;\n' +
+				'})();'
 			);
 		});
 
@@ -30,9 +38,12 @@ describe('#Util/CodeGenerator/ClassGenerator', () => {
 			generator.addProperty('prop1', '"value"');
 			generator.addProperty('prop2', '5');
 			expect(generator.toString()).to.be.equal(
-				'function Test() {} ' +
-				'Test.prototype.prop1 = "value"; ' +
-				'Test.prototype.prop2 = 5;'
+				'return (function() {\n' +
+					'\tfunction Test() {};\n' +
+					'\tTest.prototype.prop1 = "value";\n' +
+					'\tTest.prototype.prop2 = 5;\n' +
+					'\treturn Test;\n' +
+				'})();'
 			);
 		});
 
@@ -42,10 +53,15 @@ describe('#Util/CodeGenerator/ClassGenerator', () => {
 			generator.addMethod('method2', ['arg']);
 			generator.addMethod('method3', [], 'console.log(this);');
 			expect(generator.toString()).to.be.equal(
-				'function Test() {} ' +
-				'Test.prototype.method1 = function() {}; ' +
-				'Test.prototype.method2 = function(arg) {}; ' +
-				'Test.prototype.method3 = function() {console.log(this);};'
+				'return (function() {\n' +
+					'\tfunction Test() {};\n' +
+					'\tTest.prototype.method1 = function() {};\n' +
+					'\tTest.prototype.method2 = function(arg) {};\n' +
+					'\tTest.prototype.method3 = function() {\n' +
+						'\t\tconsole.log(this);\n' +
+					'\t};\n' +
+					'\treturn Test;\n' +
+				'})();'
 			);
 		});
 
